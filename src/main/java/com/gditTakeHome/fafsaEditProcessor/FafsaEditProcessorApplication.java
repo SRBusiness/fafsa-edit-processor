8794798f611d870a363ac7ea29c45dc94595ba8f
Sarah Read-Brown <sarahread-brown@Sarahs-MacBook-Pro.local>
2026-03-02 10:45:03 -0800

    Initial project skeletion via spring boot initializr

---
+++ src/main/java/com/gditTakeHome/fafsaEditProcessor/FafsaEditProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.gditTakeHome.fafsaEditProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FafsaEditProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FafsaEditProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gditTakeHome/fafsaEditProcessor/FafsaEditProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gditTakeHome.fafsaEditProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FafsaEditProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
